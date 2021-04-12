cd89f7275a3f6c48497e157cfde624b85a41f8db
David Blevins <david.blevins@gmail.com>
2021-04-11 18:34:00 -0700

    Update issues now reads descriptions from STDIN

--- jamira-cli/src/main/java/org/tomitribe/jamira/cli/UpdateCommand.java
+++ jamira-cli/src/main/java/org/tomitribe/jamira/cli/UpdateCommand.java
@@ -19,8 +19,11 @@ import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
 import org.tomitribe.crest.api.Command;
 import org.tomitribe.crest.api.Default;
+import org.tomitribe.crest.api.In;
 import org.tomitribe.crest.api.Option;
+import org.tomitribe.util.IO;
 
+import java.io.InputStream;
 import java.util.List;
 
 @Command("update")
@@ -29,7 +32,18 @@ public class UpdateCommand {
     //CHECKSTYLE:OFF
 
     /**
-     * Update the specified JIRA issue
+     * Update the specified JIRA issue.  The issue description can be supplied as a
+     * flag or alternatively by reading it from piped input to the command.
+     *
+     *     echo "This is the new description" | jamira update issue TOMEE-3141 --summary="New title"
+     *
+     * If both the description flag and piped input is used, the resulting description will
+     * be the contents of the flag, a new line, then the contents of the piped input.
+     * For example the following would set the description to "Hello\nWorld":
+     *
+     *     echo "World" | jamira update issue TOMEE-3141 --description="Hello"
+     *
+     *
      * @param issueKey Text key for the issue.  For example TOMEE-123
      * @param summary Text to use as the title of the issue
      * @param type The name of the issue type.  See the `list issue-types` command
@@ -46,17 +60,29 @@ public class UpdateCommand {
     public void issue(final IssueKey issueKey,
                       @Option("summary") final String summary,
                       @Option("type") final String type,
-                      @Option("description") final String description,
+                      @Option("description") String description,
                       @Option("assignee") final String assignee,
                       @Option("reporter") final String reporter,
                       @Option("priority") final String priority,
                       @Option("affected-version") final List<String> affectedVersions,
                       @Option("fix-version") final List<String> fixVersions,
                       @Option("component") final List<String> components,
+                      @In final InputStream descriptionInput,
                       @Option("account") @Default("default") final Account account) throws Exception {
 
         final Client client = account.getClient();
         final IssueRestClient issueClient = client.getIssueClient();
+
+        { // Read the description from System input
+            final String content = IO.slurp(descriptionInput);
+            if (content != null && content.length() > 0) {
+                if (description == null) {
+                    description = content;
+                } else {
+                    description += System.lineSeparator() + content;
+                }
+            }
+        }
 
         final IssueInputBuilder issue = new IssueInputBuilder();
 
